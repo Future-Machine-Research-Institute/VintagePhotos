4a86a75c225b2b60559cd2f5b72c1375db6841d2
Arrose <arrose.chen@foxmail.com>
2022-11-20 22:10:27 +0800

    ui

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="content">
+    <render-region class="render-region"></render-region>
+    <tools class="tools"></tools>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import RenderRegion from './RenderRegion.vue';
+  import Tools from './Tools.vue';
+    
+
+</script>
+
+<style scoped>
+
+.content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* flex-wrap: wrap; */
+}
+
+.render-region {
+  width: 100%;
+  height: 80%;
+  background-color: lightblue;
+}
+
+.tools {
+  width: 100%;
+  height: 20%;
+  background-color: lightcoral;
+}
+</style>
---
+++ src/components/Tools.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button class="import-button">导入</button>
+    </div>
+</template>
+  
+<script setup lang="ts">
+  
+      
+  
+</script>
+  
+<style scoped>
+
+.import-button {
+    width: 80px;
+    height: 40px;
+    background-color: lightgreen;
+}
+  
+</style>
