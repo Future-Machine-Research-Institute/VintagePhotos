620465bef2b61b163a586dbc8786b63933953e1c
Arrose <arrose.chen@foxmail.com>
2022-11-23 14:53:35 +0800

    自定义着色器

--- src/components/RenderRegion.vue
+++ src/components/RenderRegion.vue
@@ -11,10 +11,36 @@
 
     // const reader: FileReader = new FileReader()
 
+    const CopyShader = {
+        uniforms: {
+
+            'tDiffuse': { value: new Three.Texture() }
+
+        },
+
+        vertexShader: /* glsl */`
+                precision mediump float;
+                attribute vec2 uv;
+                attribute vec3 position;
+                varying vec2 vUv;
+                void main() {
+                    vUv = uv;
+                    gl_Position = vec4( position, 1.0 );
+                }`,
+
+        fragmentShader: /* glsl */`
+                precision mediump float;
+                uniform sampler2D tDiffuse;
+                varying vec2 vUv;
+                void main() {
+                    gl_FragColor = texture2D( tDiffuse, vUv );
+                }`
+    }
+
     let scene: Three.Scene
     let camera: Three.OrthographicCamera
     let render: Three.WebGLRenderer
-    let plane: Three.Mesh<Three.PlaneGeometry, Three.MeshBasicMaterial>
+    let plane: Three.Mesh<Three.PlaneGeometry, Three.RawShaderMaterial>
 
     let loader: Three.TextureLoader
 
@@ -33,19 +59,21 @@
         render.setSize(container.clientWidth, container.clientHeight)
 
         let planeGeometry = new Three.PlaneGeometry(2, 2)
-        let planeMaterial = new Three.MeshBasicMaterial()
-        plane = new Three.Mesh(planeGeometry, planeMaterial)
+        // let planeMaterial = new Three.MeshBasicMaterial()
+        let shaderMaterial = new Three.RawShaderMaterial(CopyShader)
+        plane = new Three.Mesh(planeGeometry, shaderMaterial)
 
         scene.add(plane)
 
         container.appendChild(render.domElement)
-        render.render(scene, camera)
+        // render.render(scene, camera)
     }
 
     // const load = (imagePath: string) => {
     //     loader.loadAsync(imagePath).then(texture => {
     //         console.log(texture)
-    //         plane.material.map = texture
+    //         CopyShader.uniforms.tDiffuse.value = texture
+    //         console.log(plane.geometry.attributes)
     //         plane.material.needsUpdate = true
     //         render.render(scene, camera)
     //     }).catch(error => {
@@ -63,7 +91,7 @@
             console.log(temp.height)
             loader.loadAsync(url).then(texture => {
                 windowURL.revokeObjectURL(url)
-                plane.material.map = texture
+                CopyShader.uniforms.tDiffuse.value = texture
                 plane.material.needsUpdate = true
                 render.render(scene, camera)
             }).catch(error => {
@@ -78,6 +106,7 @@
     
     onMounted(() => {
         init()
+        // load('image.jpg')
     })
 
   
